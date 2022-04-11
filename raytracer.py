49631e0095f670342fe1c61c24a1af9c64c959f7
Keetley Rate <kjr106@uclive.ac.nz>
2022-04-12 11:38:07 +1200

    still broke

--- raytracer.py
+++ raytracer.py
@@ -1,6 +1,17 @@
 
 import numpy as np
+from numpy.linalg import norm
 import matplotlib.pyplot as plt
+
+
+LIGHT_AMBIENT = np.array((0.2, 0.2, 0.2))
+F_CONST = 24
+
+def distance(p1, p2):
+	return norm(p1 - p2)
+
+def normalize(vector):
+	return vector / norm(vector)
 
 class Sphere:
 	"""Sphere object class"""
@@ -15,16 +26,14 @@ class Sphere:
 
 	def intersection(self, ray):
 		"""Return point of intersection of ray and sphere or None if they dont intersect"""
-		p = ray.dir[0]**2 + ray.dir[1]**2 + ray.dir[2]**2
-		q = 2*ray.dir[0]*(ray.r0[0] - self.centre[0]) + 2*ray.dir[1]*(ray.r0[1] - self.centre[1]) + 2*ray.dir[2]*(ray.r0[2] - self.centre[2])
-		w = (ray.r0[0] - self.centre[0])**2 + (ray.r0[1] - self.centre[1])**2 + (ray.r0[2] - self.centre[2])**2 - self.radius**2
-		if q**2 - 4 * p * w >= 0:
-			hit_t_1 = (-q + np.sqrt(q**2-4*p*w)) / 2*w
-			hit_t_2 = (-q - np.sqrt(q**2-4*p*w)) / 2*w
-			if ray.distance_from_hit_t(hit_t_1) < ray.distance_from_hit_t(hit_t_2):
-				ray.hit = ray.r0 + hit_t_1 * ray.dir
-			else:
-				ray.hit = ray.r0 + hit_t_2 * ray.dir
+		p = 2 * np.dot(ray.dir, ray.r0 - self.centre)
+		q = norm(ray.r0 - self.centre)**2 - self.radius**2
+		delta = p**2 - 4 * q
+		if delta > 0:
+			t1 = (-q + np.sqrt(delta)) / 2
+			t2 = (-q - np.sqrt(delta)) / 2
+			ray.hit = ray.r0 + min(t1, t2) * ray.dir
+
 		
 
 class Ray:
@@ -32,11 +41,11 @@ class Ray:
 
 	def __init__(self, source, direction):
 		"""Initialise ray with p0 as sourse and direction, hit postion as (0, 0, 0)"""
-		RSTEP = 10**-7
+		RSTEP = 10**-5
 		self.dir = np.array(direction) / np.linalg.norm(direction)
 		self.r0 = np.array(source) + RSTEP * self.dir
 		self.hit = None
-		self.index = -1
+		self.index = None
 		self.dist = 0
 
 
@@ -49,40 +58,37 @@ class Ray:
 		if self.hit is not None:
 			return np.linalg.norm(self.hit - self.r0)
 
+		
 
-	def trace(self, objects):
+	def closest_intersection(self, objects):
 		"""Return index of object closest intersect point"""
-		closest_index, dist = None, None
+		closest_obj, closest_dist = None, float("inf")
 		for i, obj in enumerate(objects):
 			obj.intersection(self)
 			if self.hit is not None:
 				int_dist = self.distance_from_hit_point()
-				if closest_index is None:
-					closest_index, dist = i, int_dist
-				elif dist > int_dist:
-					closest_index, dist = i, int_dist
-		if closest_index is not None:
-			return objects[closest_index].color
-		else:
-			return None
+				if closest_dist > int_dist:
+					closest_obj, closest_dist = i, int_dist
+		self.index = closest_obj
+		self.dist = closest_dist
 
 
 class RayTracedImage:
 	"""2D grid of pixels used to genourate image"""
 
-	def __init__(self, width, height, background_color):
+	def __init__(self, xmin, xmax, ymin, ymax, numdiv, eyedist, background, objects):
 		"""Initialize with a x in (-1, 1) and y in (-1/aspect, 1/aspect) in the z-y plane."""
-		ratio = float(width) / height
-		self.xmin, self.xmax = -1, 1
-		self.ymin, self.ymax = -1 / ratio, 1/ ratio
-		self.xspace = np.linspace(self.xmin, self.xmax, width)
-		self.yspace = np.linspace(self.ymin, self.ymax, height)
-		self.cellx = (self.xmax - self.xmin) / width
-		self.celly = (self.ymax - self.ymin) / height
-		self.eyepos = np.array((0, 0, 1))
-		self.eyedist = 1
-		self.background = np.array(background_color)
-		self.img_grid = np.zeros((width, height, 3))
+		self.cellx = (xmax - xmin) / numdiv
+		self.celly = (ymax - ymin) / numdiv
+		self.xmin = xmin
+		self.ymin = ymin
+		self.numdiv = numdiv
+		self.eyedist = eyedist
+		self.objects = objects
+		self.background = np.array(background)
+		self.img_grid = np.zeros((numdiv, numdiv, 3))
+		self.lightpos = np.array((0, ymax+10, 0))
+		
 
 
 	def set_pixel_color(self, i, j, color):
@@ -91,22 +97,50 @@ class RayTracedImage:
 		self.img_grid[i][j][1] = color[1]
 		self.img_grid[i][j][2] = color[2]
 
+	def trace(self, ray):
+		"""Compute color value of pixel that ray passes through"""
+		color_out = None
+		ray.closest_intersection(self.objects)
+		if ray.hit is not None:
+			obj = self.objects[ray.index]
+			shadow_ray = Ray(ray.hit, self.lightpos - ray.hit)
+			shadow_ray.closest_intersection(self.objects)
+			obj_normal = normalize(ray.hit - obj.centre)
+			too_light = normalize(self.lightpos - ray.hit)
+			if shadow_ray.hit is not None:
+				if shadow_ray.dist < distance(self.lightpos, ray.hit):
+					color_out = LIGHT_AMBIENT * obj.color
+				else:
+					color_out = LIGHT_AMBIENT * obj.color + np.dot(too_light, obj_normal) * obj.color 
+			else:
+				color_out = LIGHT_AMBIENT * obj.color + np.dot(too_light, obj_normal) * obj.color 
+			color_out = np.abs(color_out)
+		return color_out
 
-	def genourate(self, objects):
+					
+
+
+
+	def genourate(self):
 		"""Create image data given the objects in view"""
-		for i, x in enumerate(self.xspace):
+		objects = self.objects
+		zcentre = -self.eyedist
+		for i in range(self.numdiv):
 			xcentre = self.xmin + (i + 0.5) * self.cellx
-			for j, y in enumerate(self.yspace):
+			for j in range(self.numdiv):
 				ycentre = self.ymin + (j + 0.5) * self.celly
-				ray = Ray(self.eyepos, np.array((xcentre, ycentre, 0)) - self.eyepos)
-				color = ray.trace(objects)
-				if color is None:
-					color = self.background
-				self.img_grid[i][j] = color
+				ray = Ray((0, 0, 0), (xcentre, ycentre, zcentre))
+				ray.closest_intersection(objects)
+				if ray.index is not None:
+					self.img_grid[i][j] = objects[ray.index].color
+				else:
+					self.img_grid[i][j] = self.background
+				if ray.hit is not None:
+					print(f"image[{i}][{j}]={objects[ray.index].color}")
+				
 		plt.imsave("img.png", self.img_grid)
 
 
-img = RayTracedImage(500, 500, (0.5, 0.5, 0.5))
-s1 = Sphere((0, 0, -5), 1, (1, 0, 0))
-s2 = Sphere((0.5, 1, -2), 0.5, (0, 1, 0))
-img.genourate([s1, s2])
+s1 = Sphere((0, -10, -10), 5, (1, 0, 0)) 
+img = RayTracedImage(-100, 100, -200, 200, 100, 1, (0.5, 0.5, 0.5), [s1])
+img.genourate()
